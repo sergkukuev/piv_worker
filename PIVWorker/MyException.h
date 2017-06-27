10bf71888e8043a2149af20a594d825ab2d88086
serg-kukuev <serg.kukuev@yandex.ru>
2017-06-27 10:28:59 +0300

    CString lead to unicode

--- PIVWorker/MyException.h
+++ PIVWorker/MyException.h
@@ -5,7 +5,7 @@ class MyException	{
 public:
 	MyException() {};
 	virtual ~MyException() {};
-	virtual CString GetMsg() { return _T("Неизвестная ошибка!"); };
+	virtual CString GetMsg() { return L"Неизвестная ошибка!"; };
 };
 
 // Исключения класса CReaderExcel
@@ -14,23 +14,16 @@ public:
 	void setName(const CString& name) { this->name = name; }
 	virtual CString GetMsg() {
 		CString result;
-		result.Format(_T("Неверное расширение файла \"%s\"!"), name);
+		result.Format(L"Неверное расширение файла \"%s\"!", name);
 		return result;
 	};
 private:
-	CString name = (_T(""));
+	CString name = L"";
 };
 
 class AccessExcelException : public MyException {
 public:
-	void setOleError(const CString& error) { this->error = error; };
-	virtual CString GetMsg() {
-		CString result;
-		result.Format(_T("Запустить приложение Excel не удалось!\n\nОшибка: %s"), error);
-		return result;
-	};
-private:
-	CString error = _T("");
+	virtual CString GetMsg() { return L"Запустить приложение Excel не удалось!"; };
 };
 
 class ReadBookException: public MyException {
@@ -38,11 +31,11 @@ public:
 	void setParam(const CString& nameBook) { this->nameBook = nameBook; };
 	virtual CString GetMsg() {
 		CString result;
-		result.Format(_T("Открыть книгу \"%s\" не удалось!"), nameBook);
+		result.Format(L"Открыть книгу \"%s\" не удалось!", nameBook);
 		return result;
 	};
 private:
-	CString nameBook = _T("");
+	CString nameBook = L"";
 };
 
 class NotAllHeaderException : public MyException {
@@ -53,12 +46,12 @@ public:
 	};
 	virtual CString GetMsg() { 
 		CString result;
-		result.Format(_T("Не удалось найти все заголовки на листе \"%s\" в файле \"%s\"!"), nameSheet, nameBook);
+		result.Format(L"Не удалось найти все заголовки на листе \"%s\" в файле \"%s\"!", nameSheet, nameBook);
 		return result;
 	};
 private:
-	CString nameBook = _T("");
-	CString nameSheet = _T("");
+	CString nameBook = L"";
+	CString nameSheet = L"";
 };
 
 // Исключения для класса Test
@@ -68,17 +61,17 @@ public:
 	void SetName(CString name) { this->name = name; }
 	virtual CString GetMsg() 
 	{
-		CString result = _T("Параметр сигнала с неизвестной ошибкой!");
-		result.Format(_T("%s\n (Имя листа: %s, Параметр: %s)"), result, name, param);
+		CString result = L"Параметр сигнала с неизвестной ошибкой!";
+		result.Format(L"%s\n (Имя листа: %s, Параметр: %s)", result, name, param);
 		return result; 
 	};
 private:
-	CString name = _T("");	// Имя листа, на котором найдена неизвестная ошибка
-	CString param = _T("");	// Параметр, который обрабатывался
+	CString name = L"";	// Имя листа, на котором найдена неизвестная ошибка
+	CString param = L"";	// Параметр, который обрабатывался
 };
 
 // Исключения для класс Report
 class EmptyPathException : public MyException {
 public:
-	virtual CString GetMsg() { return _T("Путь для сохранения отчета не задан!"); };
+	virtual CString GetMsg() { return L"Путь для сохранения отчета не задан!"; };
 };
